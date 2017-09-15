79cf5fd811fceebaf0d23617c5c12ca9ecd92892
Snigdha Sur <Snigdha@Snigdhas-MacBook-Air-2.local>
2017-09-15 11:55:12 -0400

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'application#home'
+
   namespace :api do
   	namespace :v1 do
   		resources :users
